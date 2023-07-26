69b12071f280df0a96693dc9c581a2d9144f4c3b
andrescaro16 <julicaro2003@gmail.com>
2023-07-26 15:49:04 -0500

    Variables

---
+++ 2_Variables-PrintOutput/Variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+    
+    int number, secondNumber = 5;
+    printf("Number: %d\n", number);
+    printf("Second number: %d\n", secondNumber);
+
+    int age = 17;
+    printf("Age: %d\n", age);
+
+    age = 26;
+    printf("New age: %d\n", age);
+
+
+    return 0;
+}
